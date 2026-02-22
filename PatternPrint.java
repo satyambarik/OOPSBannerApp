5f0a8687f6635779e87a320f4acfec41a769f223
satyambarik <satyambarik1404@gmail.com>
2026-02-22 21:28:56 +0530

    UC2

---
+++ PatternPrint.java
@@ -0,0 +1,16 @@
+public class PatternPrint {
+	
+    public static void main(String[] args) {
+
+        System.out.println("    ***        ***        ******         *****   ");
+        System.out.println("  **   **    **   **      **    **      **    ** ");
+        System.out.println(" **     **  **     **     **    **     **     ** ");
+        System.out.println(" **     **  **     **     **    **      **       ");
+        System.out.println(" **     **  **     **     *******         ***    ");
+        System.out.println(" **     **  **     **     **                 **  ");
+        System.out.println(" **     **  **     **     **         **      **");
+        System.out.println("  **   **    **   **      **           **    ** ");
+        System.out.println("    ***        ***        **            ***** ");
+
+    }
+}
