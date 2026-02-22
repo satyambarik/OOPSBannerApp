3e10e476ee8d7e46cdfb9bd93b296f823983a830
satyambarik <satyambarik1404@gmail.com>
2026-02-22 22:44:34 +0530

    UC4

--- OOPSBannerApp.java
+++ OOPSBannerApp.java
@@ -1,24 +1,12 @@
 
 public class OOPSBannerApp{
-
-    public static void main(String[] args) {
-
-        String[][] pattern = {
-			{"    ***        ***        ******         *****    "},
-			{"  **   **    **   **      **    **      **    **  "},
-			{" **     **  **     **     **    **     **      ** "},
-			{" **     **  **     **     **    **      **        "},
-			{" **     **  **     **     *******         ***     "},
-			{" **     **  **     **     **                 **   "},
-			{" **     **  **     **     **          **      **  "},
-			{"  **   **    **   **      **           **     **  "},
-			{"    ***        ***        **             *****    "},
-            
-        };
-
-        
-        for (String[] row : pattern) {
-            System.out.println(String.join("   ", row));
-        }
+ public static void main(String[] args){
+     System.out.println("   ***        ***     ******   ******");
+     System.out.println(" **   **    **   **   **   **  **  **");
+     System.out.println("**     **  **     **  **   **  **    ");
+     System.out.println("**     **  **     **  ******   ******");
+     System.out.println("**     **  **     **  **           **");
+     System.out.println(" **   **    **   **   **       **  **");
+     System.out.println("   ***        ***     **       ******");
     }
 }
